82ee382be6325bcfdab9a532174e161211f12ec0
ingridrios23 <carolingrid.2320@gmail.com>
2025-05-23 00:24:59 -0400

    Delete chamber/scripts directory

--- chamber/scripts/main.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.getElementById("menuBtn");
-const navList = document.querySelector("nav ul");
-
-menuBtn.addEventListener("click", () => {
-  navList.classList.toggle("hidden");
-});
