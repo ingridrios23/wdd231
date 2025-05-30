172e385c42d3d475bba4aaf27173e3052fba6f12
ingridrios23 <carolingrid.2320@gmail.com>
2025-05-30 01:45:07 -0400

    Delete chamber directory

--- chamber/scripts/menu.js
+++
@@ -1,8 +0,0 @@
-const menuBtn = document.getElementById('menuBtn');
-const navUl = document.querySelector('nav ul');
-
-menuBtn.addEventListener('click', () => {
-  navUl.classList.toggle('hidden');
-  const expanded = menuBtn.getAttribute('aria-expanded') === 'true' || false;
-  menuBtn.setAttribute('aria-expanded', !expanded);
-});
