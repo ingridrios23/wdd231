14e0e4fa5c67aff2b185867cde003703ad055b1f
ingridrios23 <carolingrid.2320@gmail.com>
2025-07-28 22:55:04 -0400

    Delete chamber directory

--- chamber/scripts/menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const toggle = document.getElementById("menu-toggle");
-  const nav = document.querySelector("#primary-nav ul");
-
-  toggle.addEventListener("click", () => {
-    nav.classList.toggle("show");
-  });
-});
